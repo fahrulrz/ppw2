584237c238a2feb4243692856fd18d921da532eb
rzi7 <mfahrulrazi695@gmail.com>
2024-09-09 17:10:50 +0700

    menambah nyoba

---
+++ lat-4/database/migrations/2024_09_05_024216_post_migration.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function index(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('content');
+            $table->timestamps();
+        });
+
+        DB::table('posts')->insert([
+            'title' => 'My first post',
+            'content' => 'This is my first post'
+        ]);
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
